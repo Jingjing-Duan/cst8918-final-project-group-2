e075a0c6fd85b885caab0eaeabc2a23739d5e3ef
Jingjing Duan <duan0029@algonquinlive.com>
2026-08-11 07:03:41 -0400

    Add Terraform remote backend infrastructure

---
+++ terraform/backend/variables.tf
@@ -0,0 +1,22 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "canadacentral"
+}
+
+variable "resource_group_name" {
+  description = "Resource group for Terraform remote state"
+  type        = string
+  default     = "cst8918-finalproject-g2-backend-rg"
+}
+
+variable "storage_account_name" {
+  description = "Storage account for Terraform remote state"
+  type        = string
+  default     = "cst8918g2tfstate"
+}
