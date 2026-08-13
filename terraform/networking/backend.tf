0b468a262cb4dfd1a7ca6d92da512ff52991cb1b
KhalidAlgonquin <amch0001@algonquinlive.com>
2026-08-13 01:20:00 -0400

    Configure Azure backend authentication for Terraform plan

--- terraform/networking/backend.tf
+++ terraform/networking/backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    storage_account_name = "cst8918g2tfstatekh"
+    container_name       = "tfstate"
+    key                  = "networking.tfstate"
+
+    use_azuread_auth = true
+    use_oidc         = true
+  }
 }
